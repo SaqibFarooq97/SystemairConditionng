d66c8e7729f5ff976ab5f3b7a6abe61ef0faaa6b
saqib.farooq.work <saqib.farooq@semusi.com>
2023-09-06 21:54:20 +0530

    addded

--- js/mapcode.js
+++ js/mapcode.js
@@ -3,8 +3,7 @@ function initialize() {
   var mapOptions = {
 	zoom: 15,
 	scrollwheel: false,
-	center: new google.maps.LatLng(28.546303338721966, 77.30862999627661)
-  };
+	center: new google.maps.LatLng(28.542810, 77.301848)  };
 
   var map = new google.maps.Map(document.getElementById('googleMap'),
 	  mapOptions);
